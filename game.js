41542300ba5bdff8008d25551a2bc56f5bd002bf
thegurt <owensthegurt@gmail.com>
2026-07-16 19:52:05 -0400

    Create Game class with loop, update, and render methods
    Initialize Game class with main loop and methods.

--- game.js
+++ game.js
@@ -1,1 +1,54 @@
-console.log("Project Mimic Started");
+/*
+====================================
+PROJECT MIMIC
+====================================
+*/
+
+class Game {
+
+    constructor() {
+
+        console.log("Project Mimic");
+
+        this.running = true;
+
+        this.lastTime = performance.now();
+
+        this.loop();
+
+    }
+
+    loop = () => {
+
+        if (!this.running)
+            return;
+
+        const now = performance.now();
+
+        const delta = (now - this.lastTime) / 1000;
+
+        this.lastTime = now;
+
+        this.update(delta);
+
+        this.render();
+
+        requestAnimationFrame(this.loop);
+
+    }
+
+    update(delta) {
+
+        // Update everything
+
+    }
+
+    render() {
+
+        // Render everything
+
+    }
+
+}
+
+const game = new Game();
